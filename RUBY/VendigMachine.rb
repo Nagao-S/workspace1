c6f312e8b4124f18598db05990b5665b831699d9
shiduru <shiduru@DESKTOP-UKNNF5F>
2023-04-12 08:28:50 +0900

    keisyou

--- RUBY/VendigMachine.rb
+++ RUBY/VendigMachine.rb
@@ -1,22 +1,39 @@
 class VendingMachine
   #初期化
   def initialize(manufacturer_name)
+    #企業名
     @manufacturer_name = manufacturer_name
+    #自動販売機に入っている金額
     @deposit = 0
+    #自動販売機に入っているカップの数
+    @stock = 0
   end
+
+  #カップの補充
+  def add_cup(amount)
+    @stock += amount
+    @stock = 100 if @stock > 100
+  end
+
   #コインが入れられたときの挙動
   def deposit_coin(coin)
     @deposit += 100 if coin == 100
   end
   #ボタンが押されたときの挙動
   def press_button(item)
-    if @deposit >= item.price
+    #選択されたアイテムがコーヒカップの場合
+    if item.is_a?(CupCoffee) && @stock != 0 && @deposit >= item.price
+      @deposit -= item.price
+      @stock -=  1
+      item.name
+    #選択されたアイテムがコーヒカップ以外の場合
+    elsif item.is_a?(Beverage) && @deposit >= item.price
       @deposit -= item.price
       item.name
     else
       ''
     end
-  end
+  end  
 
   private
 
@@ -28,6 +45,7 @@ class VendingMachine
 
 end
 
+#アイテムクラス
 class Item
   attr_reader :name, :price
   def initialize(name,price)
@@ -36,13 +54,36 @@ class Item
   end
 end
 
-cola = Item.new('cola',150)
-cider = Item.new('cider',100)
-vending_machine = VendingMachine.new('サントリー')
+#アイテムクラスを継承した飲料クラス
+class Beverage < Item
+  def initialize(name)
+    price = case name
+    when 'cider'
+      100
+    when 'cola'
+      150
+    else
+      raise "None #{name}"
+    end
+    super(name, price)
+  end
+end
 
+#アイテムクラスを継承したカップコーヒクラス
+class CupCoffee < Item
+  def initialize(type)
+    super("#{type} cup coffee", 100)
+  end
+end
+
+#サンプルコード
+hot_cup_coffee = CupCoffee.new('hot');
+cider = Beverage.new('cider')
+vending_machine = VendingMachine.new('サントリー')
 vending_machine.deposit_coin(100)
-puts vending_machine.press_button(cola)
 vending_machine.deposit_coin(100)
-vending_machine.deposit_coin(100)
-puts vending_machine.press_button(cola)
 puts vending_machine.press_button(cider)
+
+puts vending_machine.press_button(hot_cup_coffee)
+vending_machine.add_cup(1)
+puts vending_machine.press_button(hot_cup_coffee)
