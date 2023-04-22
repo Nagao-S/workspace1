ac2c0fc1a411d032dc2dbeffa0c5ac2c5b2a9eed
shiduru <117517769+Nagao-S@users.noreply.github.com>
2023-04-23 03:24:23 +0900

    Delete main_report/spec directory

--- main_report/spec/spec_helper.rb
+++
@@ -1,9 +0,0 @@
-# spec_helper.rb
-require_relative "../black_jack_2"
-require 'rspec'
-
-RSpec.configure do |config|
-  config.color = true
-  config.tty = true
-  config.formatter = :documentation
-end
