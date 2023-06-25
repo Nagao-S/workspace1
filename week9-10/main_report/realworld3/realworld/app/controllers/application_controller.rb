1670db491d09ff9ea0588bb611dbf446ebedfed7
Nagao-S <shiduru1998@i.softbank.jp>
2023-06-25 10:15:42 +0900

    new

--- week9-10/main_report/realworld3/realworld/app/controllers/application_controller.rb
+++ week9-10/main_report/realworld3/realworld/app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
   include ActionController::RespondWith
+  # トークンの検証をスキップ
   skip_before_action :verify_authenticity_token
 end
--- week9-10/main_report/realworld3/realworld/app/controllers/articles_controller.rb
+++ week9-10/main_report/realworld3/realworld/app/controllers/articles_controller.rb
@@ -2,6 +2,7 @@ class ArticlesController < ApplicationController
   respond_to :json
   before_action :set_article, only: [:show, :update, :destroy]
 
+  # Articleオブジェクトの生成
   def create
     @article = Article.new(article_params)
 
@@ -12,10 +13,12 @@ class ArticlesController < ApplicationController
     end
   end
 
+  # JSONにレンダリング
   def show
     render json: @article
   end
 
+  # 更新
   def update
     if @article.update(article_params)
       render json: @article
@@ -24,16 +27,17 @@ class ArticlesController < ApplicationController
     end
   end
 
+  # 削除
   def destroy
     @article.destroy
   end
 
-  private
-
+  # オブジェクトを@articleにセット
   def set_article
     @article = Article.find_by(slug: params[:slug])
   end
 
+  private
   def article_params
     params.require(:article).permit(:title, :description, :body)
   end
